0a6b5f12622c7b6a7ed6f5bd356dd22520185087
KareemShaban1 <karim17196@fci.bu.edu.eg>
2024-03-22 05:21:06 +0200

    first commit

---
+++ resources/views/backend/dashboard/layouts/footer_scripts.blade.php
@@ -0,0 +1,16 @@
+<script>
+    var hostUrl = "assets";
+</script>
+<!--begin::Javascript-->
+<!--begin::Global Javascript Bundle(used by all pages)-->
+<script src="{{ asset('backend/plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('backend/js/scripts.bundle.js') }}"></script>
+<!--end::Global Javascript Bundle-->
+<!--begin::Page Vendors Javascript(used by this page)-->
+<script src="{{ asset('backend/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"></script>
+<!--end::Page Vendors Javascript-->
+<!--begin::Page Custom Javascript(used by this page)-->
+<script src="{{ asset('backend/js/custom/widgets.js') }}"></script>
+<script src="{{ asset('backend/js/custom/apps/chat/chat.js') }}"></script>
+<script src="{{ asset('backend/js/custom/modals/create-app.js') }}"></script>
+<script src="{{ asset('backend/js/custom/modals/upgrade-plan.js') }}"></script>
